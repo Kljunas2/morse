0fa4353190ad5bd2a40e74fe06ca39fc1d3a132d
Kljunas2 <miha@korenjak.si>
2020-05-05 10:37:23 +0200

    Updated documentation.

--- encode.go
+++ encode.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-// Basic wrapper function for Encoder type.
+// Encode is basic wrapper function for Encoder type.
 func Encode(s string) string {
 	enc := &Encoder{}
 	r := strings.NewReader(s)
